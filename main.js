96f0399ae6eb583eb01f452962a691a0f65ec882
asror797 <asror797shahobiddinov@gmail.com>
2021-11-29 00:51:25 +0500

    deploy

---
+++ main.js
@@ -0,0 +1,353 @@
+var movies = [
+  {
+    "title": "Patton Oswalt: Annihilation",
+    "year": 2017,
+    "categories": [
+      "Uncategorized"
+    ],
+    "imdbId": "tt7026230",
+    "imdbRating": 7.4,
+    "runtime": 66,
+    "language": "English",
+    "youtubeId": "4hZi5QaMBFc",
+    "summary": "Patton Oswald, despite a personal tragedy, produces his best standup yet. Focusing on the tribulations of the Trump era and life after the loss of a loved one, Patton Oswald continues his journey to contribute joy to the world.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/4hZi5QaMBFc/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/4hZi5QaMBFc/maxresdefault.jpg"
+  },
+  {
+    "title": "New York Doll",
+    "year": 2005,
+    "categories": [
+      "Documentary",
+      "Music"
+    ],
+    "imdbId": "tt0436629",
+    "imdbRating": 7.9,
+    "runtime": 75,
+    "language": "English",
+    "youtubeId": "jwD04NsnLLg",
+    "summary": "A recovering alcoholic and recently converted Mormon, Arthur \"Killer\" Kane, of the rock band The New York Dolls, is given a chance at reuniting with his band after 30 years.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/jwD04NsnLLg/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/jwD04NsnLLg/maxresdefault.jpg"
+  },
+  {
+    "title": "And Then I Go",
+    "year": 2017,
+    "categories": [
+      "Drama"
+    ],
+    "imdbId": "tt2018111",
+    "imdbRating": 7.6,
+    "runtime": 99,
+    "language": "English",
+    "youtubeId": "8CdIiD6-iF0",
+    "summary": "In the cruel world of junior high, Edwin suffers in a state of anxiety and alienation alongside his only friend, Flake. Misunderstood by their families and demoralized at school daily, their fury simmers quietly until an idea for vengeance offers them a terrifying release. Based on the acclaimed novel \"Project X\" by Jim Shepard, this unflinching look at adolescence explores how the powerful bonds of childhood friendship and search for belonging can become a matter of life or death.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/8CdIiD6-iF0/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/8CdIiD6-iF0/maxresdefault.jpg"
+  },
+  {
+    "title": "An Extremely Goofy Movie",
+    "year": 2000,
+    "categories": [
+      "Animation",
+      "Comedy",
+      "Family",
+      "Sport"
+    ],
+    "imdbId": "tt0208185",
+    "imdbRating": 6.4,
+    "runtime": 79,
+    "language": "English",
+    "youtubeId": "H8oSvldAGfg",
+    "summary": "It's a big time in Max's life. He's college bound with his friends and finally free of his embarrassing father as he strives to be a top contender for the X-Games. Unfortunately, Goofy loses his job and learns that he cannot get another job without a college degree. To his son's mortification, Goofy decides to join him in his campus to get that degree. Desperate to distract his father, Max talks him into joining the competing Gamma Fraternity team and introduces him to a wonderful librarian who shares his nostalgic love for 1970's pastimes. Unfortunately, things do not go according to plan as events put this father-son relationship to the test.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/H8oSvldAGfg/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/H8oSvldAGfg/maxresdefault.jpg"
+  },
+  {
+    "title": "Peter Rabbit",
+    "year": 2018,
+    "categories": [
+      "Adventure",
+      "Animation",
+      "Comedy",
+      "Family",
+      "Fantasy"
+    ],
+    "imdbId": "tt5117670",
+    "imdbRating": 6.6,
+    "runtime": 95,
+    "language": "English",
+    "youtubeId": "7Pa_Weidt08",
+    "summary": "Based on the books by Beatrix Potter: Peter Rabbit (James Corden;) his three sisters: Flopsy (Margot Robbie,) Mopsy (Elizabeth Debicki) and Cotton Tail (Daisy Ridley) and their cousin Benjamin (Colin Moody) enjoy their days harassing Mr McGregor in his vegetable garden. Until one day he dies and no one can stop them roaming across his house and lands for a full day or so. However, when one of Mr McGregor's relatives inherits the house and goes to check it out, he finds much more than he bargained for. What ensues, is a battle of wills between the new Mr McGregor and the rabbits. But when he starts to fall in love with Bea (Rose Byrne,) a real lover of all nature, his feelings towards them begin to change. But is it too late?",
+    "smallThumbnail": "http://i3.ytimg.com/vi/7Pa_Weidt08/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/7Pa_Weidt08/maxresdefault.jpg"
+  },
+  {
+    "title": "Love Songs",
+    "year": 2007,
+    "categories": [
+      "Uncategorized"
+    ],
+    "imdbId": "tt0996605",
+    "imdbRating": 7.2,
+    "runtime": 91,
+    "language": "English",
+    "youtubeId": "s54vpKAFmS0",
+    "summary": "Julie's boyfriend Ismaël lives with her; rather than worry about the time he spends with his colleague Alice, Julie invites Alice to join them. The three walk the streets of Paris, party, read, and sleep together. Sometimes it's lighthearted, sometimes there are jealousies. Then death strikes. In various ways, those left come to terms with the departure and absence of a loved one: showing concern, eating together, attempting new relationships, trying to \"be there\" for the other. Then, the spirit returns and new commitments are possible. The romantic elements of musical comedy play in contrast to the ambivalence of the lyrics and the story.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/s54vpKAFmS0/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/s54vpKAFmS0/maxresdefault.jpg"
+  },
+  {
+    "title": "89",
+    "year": 2017,
+    "categories": [
+      "Uncategorized"
+    ],
+    "imdbId": "tt7614404",
+    "imdbRating": 8.1,
+    "runtime": 91,
+    "language": "English",
+    "youtubeId": "5hfAExhHTMM",
+    "summary": "89 tells the incredible story of one of football's greatest triumphs: when against all odds Arsenal snatched the Championship title from Liverpool at Anfield in the last minute of the last game of the 1988/89 season. It's a universal tale of a band of brothers who, led by a charismatic and deeply respected manager, came together to defy the odds and create history. Mixing archive and previously unseen footage with revealing interviews, insights and memories from the original squad, game officials, famous fans and the people who were there on the night this is the definitive account of a watershed moment in football and a must-watch for any sports fan.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/5hfAExhHTMM/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/5hfAExhHTMM/maxresdefault.jpg"
+  },
+  {
+    "title": "The Foster Boy",
+    "year": 2011,
+    "categories": [
+      "Drama"
+    ],
+    "imdbId": "tt2057931",
+    "imdbRating": 7.4,
+    "runtime": 107,
+    "language": "English",
+    "youtubeId": "E9Qv_XVJ-js",
+    "summary": "The illegitimate orphan child, 12-year-old Max, is sold by the local minister for a basket of food to the Bösiger family, who own a mountain farm. Max' initial hope of finally finding a loving home is brutally shattered: The farmer and his wife treat Max like livestock, and their son Jacob humiliates and abuses him. Only the local teacher notices the child suffering on the farm.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/E9Qv_XVJ-js/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/E9Qv_XVJ-js/maxresdefault.jpg"
+  },
+  {
+    "title": "Forever My Girl",
+    "year": 2018,
+    "categories": [
+      "Drama",
+      "Music",
+      "Romance"
+    ],
+    "imdbId": "tt4103724",
+    "imdbRating": 6.4,
+    "runtime": 108,
+    "language": "English",
+    "youtubeId": "3vqcMr1q5Uc",
+    "summary": "After being gone for a decade a country star returns home to the love he left behind.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/3vqcMr1q5Uc/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/3vqcMr1q5Uc/maxresdefault.jpg"
+  },
+  {
+    "title": "Tom Segura: Disgraceful",
+    "year": 2018,
+    "categories": [
+      "Comedy",
+      "Documentary"
+    ],
+    "imdbId": "tt7379330",
+    "imdbRating": 7.5,
+    "runtime": 0,
+    "language": "English",
+    "youtubeId": "kYYINJM3lPA",
+    "summary": "Comedian Tom Segura rants about funny things about pop culture and the way of living in 2018.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/kYYINJM3lPA/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/kYYINJM3lPA/maxresdefault.jpg"
+  },
+  {
+    "title": "The Secret Rules of Modern Living: Algorithms",
+    "year": 2015,
+    "categories": [
+      "Documentary"
+    ],
+    "imdbId": "tt5818010",
+    "imdbRating": 7.5,
+    "runtime": 58,
+    "language": "English",
+    "youtubeId": "hpyxmT6KSvw",
+    "summary": "Professor Marcus du Sautoy demystifies the hidden world of algorithms and reveals where these 2,000-year-old problem solvers came from, how they work, and what they have achieved.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/hpyxmT6KSvw/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/hpyxmT6KSvw/maxresdefault.jpg"
+  },
+  {
+    "title": "Secrets in the Fall",
+    "year": 2015,
+    "categories": [
+      "Family"
+    ],
+    "imdbId": "tt2522908",
+    "imdbRating": 7.3,
+    "runtime": 0,
+    "language": "English",
+    "youtubeId": "eMVw1pHVMAw",
+    "summary": "Nothing is impossible for a group of young campers, friends and enemies alike, on a weekend retreat at Camp Pinnacle. But what starts as an exciting wilderness adventure turns into trouble when a young camper goes missing and the rest of the group must set out to find him. The campers are forced to overcome their differences and band together to re-claim their weekend and make it out of the woods before dark!",
+    "smallThumbnail": "http://i3.ytimg.com/vi/eMVw1pHVMAw/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/eMVw1pHVMAw/maxresdefault.jpg"
+  },
+  {
+    "title": "The Boy Next Door",
+    "year": 2015,
+    "categories": [
+      "Action",
+      "Mystery",
+      "Romance",
+      "Thriller"
+    ],
+    "imdbId": "tt3181822",
+    "imdbRating": 4.7,
+    "runtime": 91,
+    "language": "English",
+    "youtubeId": "YrE97rY7ZKo",
+    "summary": "When a handsome, charming teenager named Noah (Ryan Guzman) moves in next door, newly separated high-school teacher Claire Peterson (Jennifer Lopez) encourages his friendship and engages in a little bit of harmless (or so she thinks) flirtation. Although Noah spends much of the time hanging out with Claire's son, the teen's attraction to her is palpable. One night, Claire gives in to temptation and lets Noah seduce her, but when she tries to end the relationship, he turns violent.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/YrE97rY7ZKo/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/YrE97rY7ZKo/maxresdefault.jpg"
+  },
+  {
+    "title": "USS Indianapolis: Men of Courage",
+    "year": 2016,
+    "categories": [
+      "Action",
+      "Drama",
+      "History",
+      "Thriller",
+      "War"
+    ],
+    "imdbId": "tt2032572",
+    "imdbRating": 5.2,
+    "runtime": 128,
+    "language": "English",
+    "youtubeId": "ZDPE-NronKk",
+    "summary": "The harrowing true story of the crew of the USS Indianapolis, who were stranded in the Philippine Sea for five days after delivering the atomic weapons that would eventually end WWII. As they awaited rescue, they endured extreme thirst, hunger, and relentless shark attacks.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/ZDPE-NronKk/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/ZDPE-NronKk/maxresdefault.jpg"
+  },
+  {
+    "title": "Security",
+    "year": 2017,
+    "categories": [
+      "Action",
+      "Crime",
+      "Thriller"
+    ],
+    "imdbId": "tt3501112",
+    "imdbRating": 5.7,
+    "runtime": 88,
+    "language": "English",
+    "youtubeId": "KbKAkL-tmSY",
+    "summary": "An ex-special services veteran (Antonio Banderas), down on his luck and desperate for work, takes a job as a security guard at a run-down mall in a rough area of town. On his first night on the job, he opens the doors up to a distraught and desperate young girl who has escaped and fled from a hijacking of the US Marshal motorcade that was transporting her to testify as a trial witness. Hot on her heels is psychopathic hijacker (Ben Kingsley), alongside his resourceful henchmen, who will stop at nothing to extract and eliminate their witness.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/KbKAkL-tmSY/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/KbKAkL-tmSY/maxresdefault.jpg"
+  },
+  {
+    "title": "A Good Day to Die Hard",
+    "year": 2013,
+    "categories": [
+      "Action",
+      "Crime",
+      "Thriller"
+    ],
+    "imdbId": "tt1606378",
+    "imdbRating": 5.3,
+    "runtime": 98,
+    "language": "English",
+    "youtubeId": "0_0K7ZfU2e0",
+    "summary": "Iconoclastic, take-no-prisoners cop John McClane, for the first time, finds himself on foreign soil after traveling to Moscow to help his wayward son Jack - unaware that Jack is really a highly-trained CIA operative out to stop a nuclear weapons heist. With the Russian underworld in pursuit, and battling a countdown to war, the two McClanes discover that their opposing methods make them unstoppable heroes.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/0_0K7ZfU2e0/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/0_0K7ZfU2e0/maxresdefault.jpg"
+  },
+  {
+    "title": "All Eyez on Me",
+    "year": 2017,
+    "categories": [
+      "Biography",
+      "Drama",
+      "Music"
+    ],
+    "imdbId": "tt1666185",
+    "imdbRating": 5.8,
+    "runtime": 139,
+    "language": "English",
+    "youtubeId": "nsrOCzUwcjE",
+    "summary": "Tells the true and untold story of prolific rapper, actor, poet and activist Tupac Shakur. The film follows Shakur from his early days in New York City to his evolution into being one of the world's most recognized and influential voices before his untimely death at the age of 25. Against all odds, Shakur's raw talent, powerful lyrics and revolutionary mind-set propelled him into becoming a cultural icon whose legacy continues to grow long after his passing. ALL EYEZ ON ME stars Kat Graham, Lauren Cohan, Hill Harper, Jamal Woolard, Danai Gurira and Demetrius Shipp Jr. as Tupac Shakur.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/nsrOCzUwcjE/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/nsrOCzUwcjE/maxresdefault.jpg"
+  },
+  {
+    "title": "Aloha",
+    "year": 2015,
+    "categories": [
+      "Action",
+      "Comedy",
+      "Drama",
+      "Romance"
+    ],
+    "imdbId": "tt1243974",
+    "imdbRating": 5.4,
+    "runtime": 105,
+    "language": "English",
+    "youtubeId": "O3mf_ewjc7s",
+    "summary": "A celebrated military contractor returns to the site of his greatest career triumphs - the US Space program in Honolulu, Hawaii - and reconnects with a long-ago love while unexpectedly falling for the hard-charging Air Force watchdog assigned to him.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/O3mf_ewjc7s/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/O3mf_ewjc7s/maxresdefault.jpg"
+  },
+  {
+    "title": "Aftermath",
+    "year": 2017,
+    "categories": [
+      "Drama",
+      "Thriller"
+    ],
+    "imdbId": "tt4581576",
+    "imdbRating": 5.7,
+    "runtime": 94,
+    "language": "English",
+    "youtubeId": "ZN8toxhSn9Y",
+    "summary": "Two strangers' lives become inextricably bound together after a devastating plane crash. Inspired by actual events, AFTERMATH tells a story of guilt and revenge after an air traffic controller's (Scoot McNairy) error causes the death of a construction foreman's (Arnold Schwarzenegger) wife and daughter.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/ZN8toxhSn9Y/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/ZN8toxhSn9Y/maxresdefault.jpg"
+  },
+  {
+    "title": "Point Break",
+    "year": 2015,
+    "categories": [
+      "Action",
+      "Crime",
+      "Sport",
+      "Thriller"
+    ],
+    "imdbId": "tt2058673",
+    "imdbRating": 5.3,
+    "runtime": 114,
+    "language": "English",
+    "youtubeId": "ncvFAm4kYCo",
+    "summary": "A young FBI agent infiltrates an extraordinary team of extreme sports athletes he suspects of masterminding a string of unprecedented, sophisticated corporate heists. Deep undercover, and with his life in danger, he strives to prove these athletes are the architects of the mind-boggling crimes that are devastating the world's financial markets.",
+    "smallThumbnail": "http://i3.ytimg.com/vi/ncvFAm4kYCo/hqdefault.jpg",
+    "bigThumbnail": "http://i3.ytimg.com/vi/ncvFAm4kYCo/maxresdefault.jpg"
+  }
+]
+
+movies.map(movie =>{
+  const box = document.createElement("div")
+        box.classList.add("box")
+  const img = document.createElement("img")
+        img.classList.add("img")
+        img.setAttribute("src", movie.smallThumbnail)
+  const title = document.createElement("p")
+        title.classList.add("title")
+        title.textContent=movie.title
+  const lang = document.createElement("p")
+        lang.classList.add("lang")
+        lang.textContent=movie.language
+        
+
+  box.appendChild(img)
+  box.appendChild(title)
+  box.appendChild(lang)
+  document.querySelector(".movies").appendChild(box)
+})
